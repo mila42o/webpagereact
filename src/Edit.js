ec989c4148d6ee4a149ee31b327a41ac28f6529a
mila42o <mila.dacheva@gmail.com>
2024-09-05 13:59:30 +0300

    css pipan

--- src/Edit.js
+++ src/Edit.js
@@ -10,21 +10,21 @@ const Edit = ({ projectid, onCancel }) => {
         <h2>Project Name:{projectid}</h2>
         <hr />
             <div className="items">
-                <img className="hiksche" src={one} alt="editicon" />
+                <img className="ikonki2" src={one} alt="editicon" />
                 <p className="inf" >Edit project name</p>
             </div>
             <hr />
             <div className="items">
-                <img className="hiksche" src={two} alt="editusericon" />
+                <img className="ikonki" src={two} alt="editusericon" />
                 <p className="inf" >Invite user</p>
             </div>
             <hr />
             <div className="items">
-                <img className="hiksche" src={three} alt="deleteicon" />
+                <img className="ikonki" src={three} alt="deleteicon" />
                 <p className="inf" >Delete project</p>
             </div>
             <hr />
-            <p className="linkowe2" onClick={onCancel}>open other project</p>
+            <p className="linkowe4" onClick={onCancel}>open other project</p>
     </div>
   );
 }
